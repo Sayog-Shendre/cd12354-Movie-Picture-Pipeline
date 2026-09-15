1c70ccbf36f5aa649079d896754d566f896202ff
Sayog-Shendre <sayogshendre6838@gmail.com>
2026-09-15 12:12:43 +0000

    fix: define apiUrl with fallback in components

--- starter/frontend/src/components/MovieDetails.js
+++ starter/frontend/src/components/MovieDetails.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 
 function MovieDetail({ movie }) {
   const [details, setDetails] = useState(null);
+
   useEffect(() => {
+    const apiUrl = process.env.REACT_APP_MOVIE_API_URL || '/proxy/5000';
     axios.get(`${apiUrl}/movies/${movie.id}`).then((response) => {
       setDetails(response.data);
     });
--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -1,29 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import axios from 'axios';
 
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
+    const apiUrl = process.env.REACT_APP_MOVIE_API_URL || '/proxy/5000';
     axios.get(`${apiUrl}/movies`).then((response) => {
       setMovies(response.data.movies);
     });
   }, []);
 
   return (
-    <ul>
+    <div>
       {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
-          {movie.title}
-        </li>
+        <div key={movie.id} onClick={() => onMovieClick(movie)}>
+          <h3>{movie.title}</h3>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 }
-
-MovieList.propTypes = {
-  onMovieClick: PropTypes.func.isRequired,
-};
 
 export default MovieList;
